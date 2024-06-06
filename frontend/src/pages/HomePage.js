03380bcff5eafff65d858415cc37aa04857c1c1f
Burak Aksoy <burakaksoy@Burak-MacBook-Pro.local>
2024-06-06 17:10:45 +0300

    Homepage created.

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h3>MerdUnal § BörekAksoy creation.</h3>
+            <div>
+                <p>Lorem ipsum.</p>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
