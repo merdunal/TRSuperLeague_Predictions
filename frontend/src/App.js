77b25c9c50700e2db1431a726b0230538c894895
Mert ÜNAL <mertunal878@gmail.com>
2024-06-07 18:04:43 +0300

    added wallet connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/HomePage"; // Adjust the path if necessary
+=======
+import Header from "./components/Header";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <HomePage />
+=======
+      <Header />
+>>>>>>> Stashed changes
     </div>
   );
 }
